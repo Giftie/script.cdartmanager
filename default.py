744dc09d1c2ee7334af78e41da1deac8061efadc
giftie61 <giftie61@9d839f9a-b7eb-e670-9fec-fcfc41fa66c4>
2010-07-06 08:23:10 +0000

    update version number...

--- default.py
+++ default.py
@@ -1,12 +1,11 @@
 __scriptname__    = "CDArt Manager Script"
 __scriptID__      = "script.cdartmanager"
 __author__        = "Giftie"
-__version__       = "0.8.0"
+__version__       = "0.8.5"
 __XBMC_Revision__ = "30001"
 __date__          = "05-06-10"
 import sys
 import os
-import xbmcgui
 import xbmcaddon
 import xbmc
 
